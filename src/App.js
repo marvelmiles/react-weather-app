420ad8f7385081af09f3ee6ff77e2f66a6b9036d
Akinrinmola marvellous <marvellousabidemi2@gmail.com>
2021-07-15 21:31:35 +0100

    hello worldssdd

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = ()=>{
         <>
         <div className="main-container">
  
-            <h1 className="title">you work</h1>
+            <h1 className="title">you workefeq</h1>
             <input 
             type="text"
             className="search"
